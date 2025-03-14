a8beff27ffa629ca07ca11d762a53c9ebedd1e20
JACaddock <jamesacaddock@gmail.com>
2025-03-14 10:47:39 +0000

    Created the repositories for the database

---
+++ Server/Repositories/RDS/RDSSlimeRepository.cs
@@ -0,0 +1,42 @@
+﻿using Server.Models;
+
+namespace Server.Repositories.RDS
+{
+    public class RDSSlimeRepository : ISlimeRepository
+    {
+        public bool Add(Slime slime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Slime slime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Slime> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Slime> GetAllMarket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Slime? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Slime> GetByOwner(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Slime slime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server/Repositories/RDS/RDSUserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Server.DTO;
+using Server.Models;
+
+namespace Server.Repositories.RDS
+{
+    public class RDSUserRepository : IUserRepository
+    {
+        public bool Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserUnique> GetAllUnique()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
